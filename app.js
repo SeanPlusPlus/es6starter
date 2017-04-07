fb3bfa1933cf31c90d95e56eee59710a3672f663
Sean Stephenson <sms@pixar.com>
2017-04-07 10:47:56 -0700

    add err handler

--- app.js
+++ app.js
@@ -4,26 +4,29 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-log(`Hitting the Slack API now :star: :star: :star:`);
+const SLACK_WEBHOOK_URL = process.env.SLACK_WEBHOOK_URL
+if (SLACK_WEBHOOK_URL === undefined) {
+  log('Create a .env file and add a value for SLACK_WEBHOOK_URL', 'warn')
+  process.exit()
+}
 
 const ts = Math.floor(Date.now() / 1000)
+const fields = [{
+  title: 'Priority',
+  value: 'High',
+  short: true,
+}]
 const attachments = [{
+  ts,
+  fields,
   fallback: 'Required plain-text summary of the attachment.',
   color: '#36a64f',
   pretext: 'Optional text that appears above the attachment block',
   title: 'Slack API Documentation',
   title_link: 'https://api.slack.com/',
   text: 'Optional text that appears within the attachment',
-  fields: [
-    {
-      title: 'Priority',
-      value: 'High',
-      short: false,
-    },
-  ],
   footer: 'Slack API',
   footer_icon: 'https://platform.slack-edge.com/img/default_application_icon.png',
-  ts,
 }]
 
 const username = 'webhookbot'
@@ -34,13 +37,12 @@ const body = {
   attachments,
 }
 
-const uri = process.env.SLACK_WEBHOOK_URL
 const method = 'POST'
 const options = {
   method,
-  uri,
   body,
   json: true,
+  uri: SLACK_WEBHOOK_URL,
 }
 
 request(options, (err, response, body) => {
